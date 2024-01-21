561a926d88c0bff270a5c8c3b197e5e3520f16c8
Huotes <aureliodosol@gmail.com>
2024-01-21 20:32:12 -0300

    [POR-001] Adição do player como imagem estatica

--- level.py
+++ level.py
@@ -1,6 +1,22 @@
 import pygame
 from settings import *
+from player import Player
 
 class Level:
-    def run(self, dt):
-        print('run game')
+    def __init__(self):
+        
+        #pega a primeira camada do display
+        self.display_surface = pygame.display.get_surface()
+
+        #grupo de sprites
+        self.all_sprites = pygame.sprite.Group()
+
+        self.setup()
+
+    def setup(self):
+        self.player = Player((640,360), self.all_sprites)
+
+    def run(self,dt):
+        self.display_surface.fill('black')
+        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.update()
