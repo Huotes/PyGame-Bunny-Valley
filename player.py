93f0bb954dd9b3d17ccfe381759c3252d20caaf7
Huotes <aureliodosol@gmail.com>
2024-01-22 19:45:34 -0300

    [POR-002] Cria setup de movimentação para o jogador

--- player.py
+++ player.py
@@ -5,6 +5,47 @@ class Player(pygame.sprite.Sprite):
     def __init__(self,pos,group):
         super().__init__(group)
 
-        self.image = pygame.Surface((64,32))
+        #atribuições gerais
+        self.image = pygame.Surface((32,64))
         self.image.fill('green')
         self.rect = self.image.get_rect(center = pos)
+
+        #atribuições de movimento
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def move(self,dt):
+
+        #Normalizando um vetor para deixar a velocidade de movimento linear
+        if self.direction.magnitude() > 0:
+            self.direction = self.direction.normalize()
+
+        #Movimentação horizontal
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+
+        #Movimentação vertical
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
