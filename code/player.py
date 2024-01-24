67c2dba8dd327c30ebced4031f8b67a68cd40866
Huotes <aureliodosol@gmail.com>
2024-01-23 21:54:34 -0300

    [POR-004] Adiciona animacao de movimento ao jogador

--- code/player.py
+++ code/player.py
@@ -3,64 +3,82 @@ from settings import *
 from support import *
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self, pos, group):
-		super().__init__(group)
+    def __init__(self, pos, group):
+        super().__init__(group)
 
-		self.import_assets()
-		self.status = 'down_idle'
-		self.frame_index = 0
+        self.import_assets()
+        self.status = 'down_idle'
+        self.frame_index = 0
 
-		# general setup
-		self.image = self.animations[self.status][self.frame_index]
-		self.rect = self.image.get_rect(center = pos)
+        # general setup
+        self.image = self.animations[self.status][self.frame_index]
+        self.rect = self.image.get_rect(center=pos)
 
-		# movement attributes
-		self.direction = pygame.math.Vector2()
-		self.pos = pygame.math.Vector2(self.rect.center)
-		self.speed = 200
+        # movement attributes
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
 
-	def import_assets(self):
-		self.animations = {'up':[],'down': [],'left':[],'right': [],
-						   'right_idle':[],'left_idle':[],'up_idle':[],'down_idle':[],
-						   'right_hoe':[],'left_hoe':[],'up_hoe':[],'down_hoe':[],
-						   'right_axe':[],'left_axe':[],'up_axe':[],'down_axe':[],
-						   'right_water':[],'left_water':[],'up_water':[],'down_water':[]}
+    def import_assets(self):
+        self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
+                           'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
+                           'right_hoe': [], 'left_hoe': [], 'up_hoe': [], 'down_hoe': [],
+                           'right_axe': [], 'left_axe': [], 'up_axe': [], 'down_axe': [],
+                           'right_water': [], 'left_water': [], 'up_water': [], 'down_water': []}
 
-		for animation in self.animations.keys():
-			full_path = 'graphics/character/' + animation
-			self.animations[animation] = import_folder(full_path)
+        for animation in self.animations.keys():
+            full_path = 'graphics/character/' + animation
+            self.animations[animation] = import_folder(full_path)
 
-	def input(self):
-		keys = pygame.key.get_pressed()
+    def animate(self, dt):
+        self.frame_index += 4 * dt
+        if self.frame_index >= len(self.animations[self.status]):
+            self.frame_index = 0
 
-		if keys[pygame.K_UP]:
-			self.direction.y = -1
-		elif keys[pygame.K_DOWN]:
-			self.direction.y = 1
-		else:
-			self.direction.y = 0
+        self.image = self.animations[self.status][int(self.frame_index)]
 
-		if keys[pygame.K_RIGHT]:
-			self.direction.x = 1
-		elif keys[pygame.K_LEFT]:
-			self.direction.x = -1
-		else:
-			self.direction.x = 0
+    def input(self):
+        keys = pygame.key.get_pressed()
 
-	def move(self,dt):
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+            self.status = 'up'
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+            self.status = 'down'
+        else:
+            self.direction.y = 0
 
-		# normalizing a vector 
-		if self.direction.magnitude() > 0:
-			self.direction = self.direction.normalize()
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+            self.status = 'right'
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+            self.status = 'left'
+        else:
+            self.direction.x = 0
 
-		# horizontal movement
-		self.pos.x += self.direction.x * self.speed * dt
-		self.rect.centerx = self.pos.x
+    def get_status(self):
+        #se o jogador nao esta se movendo:
+        if self.direction.magnitude() == 0:
+            #adiciona _idle ao status
+            self.status = self.status.split('_')[0] + '_idle'
 
-		# vertical movement
-		self.pos.y += self.direction.y * self.speed * dt
-		self.rect.centery = self.pos.y
+    def move(self, dt):
+        # normalizing a vector
+        if self.direction.magnitude() > 0:
+            self.direction = self.direction.normalize()
 
-	def update(self, dt):
-		self.input()
-		self.move(dt)
+        # horizontal movement
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+
+        # vertical movement
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
+
+    def update(self, dt):
+        self.input()
+        self.get_status()
+        self.move(dt)
+        self.animate(dt)
